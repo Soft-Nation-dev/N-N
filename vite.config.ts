1872a306f64d727d7ecf83a756519812ef412b86
Soft-Nation-dev <emmanuelsoft8105@gmail.com>
2026-08-17 05:28:41 +0100

    Fix custom domain asset paths

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/N-N/',
+  base: '/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
